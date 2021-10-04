18ebbdccf013e42ab305e6d587985efe557550c3
Luong Ha Vinh <luonghavinh3011@gmail.com>
2021-10-04 08:09:46 +0700

    update js, update import model

--- models/importOrderModel.php
+++ models/importOrderModel.php
@@ -16,13 +16,17 @@ class ImportOrder
 	public function CreateNew($user){
 		include './database/db_connect.php';
 		$dateCreated = date("Y-m-d H:i:s");
-		$result = mysqli_query($conn,"INSERT INTO importorder (user,dateCreated) VALUES ('".$user."','".$dateCreated."')");
+		$result = mysqli_query($conn,"INSERT INTO importorder (account_id,dateCreated) VALUES ('".$user."','".$dateCreated."')");
+		if ($result === TRUE) {
+  			$last_id = $conn->insert_id;
+  			$detail = mysqli_query($conn,"INSERT INTO detailImport (idorder) VALUES ('".$last_id."')");
+  		}
 	}
 	public function DeleteOrder($id){
 			include './database/db_connect.php';
 			$result = mysqli_query($conn,"DELETE FROM importorder Where id ='".$id."'");
       		header('location:importOrder.php');
       		mysqli_close($conn);
- 		}
+ 	}
 }
 ?>
