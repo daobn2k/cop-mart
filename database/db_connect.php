e969cf54cf0e0fe21a649284c25c03694015bcc9
Luong Ha Vinh <luonghavinh3011@gmail.com>
2021-10-04 10:37:04 +0700

    Update db_connect.php
    xóa echo mỗi lần kết nối db

--- database/db_connect.php
+++ database/db_connect.php
@@ -8,6 +8,5 @@
     $db = "cop_mart";
     $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
-    echo("Dang nhap thanh cong")
     //db connect
 ?>
